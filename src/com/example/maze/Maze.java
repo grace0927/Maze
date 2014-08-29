e31a02534aa13a542601335b0dc698904701df30
jfeng0927 <jfeng@seawolftech.com>
2014-08-29 18:01:51 -0400

    run

--- src/com/example/maze/Maze.java
+++ src/com/example/maze/Maze.java
@@ -1,5 +1,7 @@
 package com.example.maze;
 
+import java.util.*;
+
 public class Maze {
 
 	Graph maze;
@@ -11,9 +13,23 @@ public class Maze {
 		for(int i=0; i<3; i++) {
 			for(int j=0; j<3; j++) {
 				Vertice a = new Vertice(i, j);
+				if(i==0 && j==0) {
+					this.start = a;
+				}
+				if(i==2 && j==2) {
+					this.end = a;
+				}
 				this.maze.addVertice(a);
 			}
 		}
+	}
+	
+	public boolean addEdge(Vertice a, Vertice b){
+		if(maze.getVertice(a)!=null && maze.getVertice(b)!=null) {
+			maze.addEdge(maze.getVertice(a), maze.getVertice(b));
+			return true;
+		}
+		return false;
 	}
 	
 	public boolean setStart(Vertice a) {
@@ -32,10 +48,31 @@ public class Maze {
 		return false;
 	}
 	
+	public void run() {
+		List<Vertice> path = new LinkedList<>();
+		path = maze.findPath(this.start, this.end);
+		if(path == null) {
+			System.out.println("can not find path");
+		} else {
+			for(Vertice vertice : path) {
+				System.out.println("(" + vertice.getX() + "," + vertice.getY() + ") -> ");
+			}
+			System.out.println("end");
+		}
+	}
+	
 	public static void main(String[] args) {
 		Maze test = new Maze();
 		
+		test.addEdge(new Vertice(0,0), new Vertice(0,1));
+		test.addEdge(new Vertice(0,0), new Vertice(2,0));
+		test.addEdge(new Vertice(2,0), new Vertice(2,1));
+		test.addEdge(new Vertice(2,1), new Vertice(1,1));
+		test.addEdge(new Vertice(1,1), new Vertice(1,2));
+		test.addEdge(new Vertice(1,2), new Vertice(0,2));
+		test.addEdge(new Vertice(1,2), new Vertice(2,2));
 		
+		test.run();
 
 	}
 
